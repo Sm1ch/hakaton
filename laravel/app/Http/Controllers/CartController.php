91f3e3f2f117724a5f6f219d065a2ed1feebf239
Sm1ch <mguseynov8@gmail.com>
2023-05-21 04:01:50 +0300

    Created cart addition

--- laravel/app/Http/Controllers/CartController.php
+++ laravel/app/Http/Controllers/CartController.php
@@ -3,21 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\CartItems;
+use App\Models\Goods;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CartController extends Controller
 {
-    public function getLast(): View
-    {
+    public function getLast() {
         return view('cart', ['carts' => Cart::query()->where('isClosed', '=', 'false')->get()]);
     }
 
     public function getOrderList(): View{
         return view('cart',['carts' => Cart::query()->where('isClosed', '=','true')->get()]);
     }
-    public function addItem(int $goodId): View{
-        dd($goodId);
-//        return view();
+
+    public function addItem(int $goodId) {    
+        $cart = Cart::query()->where('isClosed', '=','false')->orderBy('id','DESC')->first();
+    
+        $good = Goods::query()->where('id', '=',$goodId)->first();
+     
+        if (empty($cart)) {
+            $cart = Cart::query()->insert([
+            'price' => $good->price,
+                'userId' =>  auth('web')->user()->id,
+                'isClosed' => false,
+            ]);
+        }
+        
+        CartItems::query()->insert(['cart_id' => $cart->id, 'good_id' => $goodId, 'good_type' => 1, 'good_ingredients' => '{}', 'price' => $good->price]);
+        return redirect()->back();
     }
 }
